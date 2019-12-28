9a5c72820d7dfe3d32036b3fe4210290b9b5e482
Shaddi H <shaddih1@gmail.com>
2019-12-28 15:35:44 -0600

    Update stegno.py

--- stegno.py
+++ stegno.py
@@ -1,30 +1,34 @@
 #!/usr/bin/env python
+# -*- coding: utf8 -*-
 
-# library
+# python standard library
 import sys, argparse, subprocess
 
 def usage():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-f", "--file", metavar="FILE_NAME",
+    parser.add_argument("-f", "--file", metavar"FILE_NAME",
     help="set a file name to hide or to extract")
     parser.add_argument("-e", "--extract", action="store_true",
     help="extract hidden text from image ")
-    if not len(sys.argv[1:]):
-        subprocess.check_call(['bash', 'stegno.sh'])
+
     return parser.parse_args()
 
 def main():
+    # parse args/options
     args = usage()
     file = args.file
-    if not args.extract:
+    if file and not a:
         text = raw_input("Text: ")
-        append = subprocess.call(['echo', '{} >> {}'.format(text, file)])
+        append = subprocess.call(['echo {} >> {}'.format(text, file)], shell=True)
         print("Text has be hidden inside the image")
     else:
         text = subprocess.call(['echo', 'strings {} | tail -1'.format(file)])
         print("Text: {}".format(extract))
 
-try:
-    main()
-except KeyboardInterrupt:
-    sys.exit(0)
+if __name__ == '__main__':
+    try:
+        main()
+    except (KeyboardInterrupt):
+        pass
+    finally:
+        print("")
