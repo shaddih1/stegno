b2f0831a4435a2324c97c9cfeb9699280cd47f81
Shaddi H <shaddih1@gmail.com>
2020-01-04 16:29:32 -0600

    Update stegno.py

--- stegno.py
+++ stegno.py
@@ -15,14 +15,14 @@ def usage():
 
 def append(file_name):
     text = raw_input("Text: ")
-    with open(file, 'a') as append:
-        append.write(text)
-    print("Text has be hidden inside the image")
+    with open(file_name, 'a') as add:
+        add.write('\n' + text)
+        print("Text has be hidden inside the image")
 
 def extract(file_name):
     with open(file_name, 'r') as text:
-        line = text.readlines()
-        extract = [line]
+        lines = text.readlines()
+        extract = [lines]
 
     return extract[-1][-1]
 
@@ -30,9 +30,9 @@ def main():
     # parse args/options
     args = usage()
     file_name = args.file
-    if not args.extract:
+    if file_name and not args.extract:
         append(file_name)
     else:
-        print("Text: {}".format(extract()))
+        print("Text: {}".format(extract(file_name)))
 
 main()
