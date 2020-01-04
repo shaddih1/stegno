1766289df808d95a6e6c848a318e3475a38cd2b4
Shaddi H <shaddih1@gmail.com>
2020-01-04 16:01:02 -0600

    Update stegno.py

--- stegno.py
+++ stegno.py
@@ -2,33 +2,37 @@
 # -*- coding: utf8 -*-
 
 # python standard library
-import sys, argparse, subprocess
+import sys, argparse
 
 def usage():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-f", "--file", metavar"FILE_NAME",
+    parser.add_argument("-f", "--file", metavar="FILE_NAME",
     help="set a file name to hide or to extract")
     parser.add_argument("-e", "--extract", action="store_true",
     help="extract hidden text from image ")
 
     return parser.parse_args()
 
+def append(file_name):
+    text = raw_input("Text: ")
+    with open(file, 'a') as append:
+        append.write(text)
+    print("Text has be hidden inside the image")
+
+def extract(file_name):
+    with open(file_name, 'r') as text;
+        line = text.readlines()
+        extract = [line]
+
+    return extract[-1][-1]
+
 def main():
     # parse args/options
     args = usage()
-    file = args.file
-    if file and not a:
-        text = raw_input("Text: ")
-        append = subprocess.call(['echo {} >> {}'.format(text, file)], shell=True)
-        print("Text has be hidden inside the image")
+    file_name = args.file
+    if not args.extract:
+        append(file_name)
     else:
-        text = subprocess.call(['echo', 'strings {} | tail -1'.format(file)])
-        print("Text: {}".format(extract))
-
-if __name__ == '__main__':
-    try:
-        main()
-    except (KeyboardInterrupt):
-        pass
-    finally:
-        print("")
+        print("Text: {}".format(extract()))
+        
+main()
